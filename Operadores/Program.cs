bdaf90728472339fea9aabafb428192c0817a039
Damian Puerta <CRistianpuerta1125>
2022-06-28 12:52:09 -0500

    Agregar archivos de proyecto.

---
+++ Operadores/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace Operadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 3;
+            int num3;
+
+            // operadores unarios 
+
+            num3 = -num1;
+            Console.WriteLine("El valor negatico es {0}", num3);
+
+            bool haySol = true;
+            Console.WriteLine("Hay sol? {0}", !haySol);
+            Console.Read();
+        }
+    }
+}
